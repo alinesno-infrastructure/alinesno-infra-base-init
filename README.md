ae71bd675a947ae427f4460019aa95601dae5d56
switchII <luoandon@gmail.com>
2023-09-29 08:57:07 +0800

    feat:集成前端UI和单点登陆

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# alinesno-infra-base-init
+# alinesno-infra-base-starter
 代码生成器和代码插件管理，应用模板生成
+
+## 鸣谢
+
+- 代码生成器引用[ruoyi](http://ruoyi.vip)代码生成器部分改造
---
+++ alinesno-infra-base-starter-boot/src/main/java/com/alinesno/infra/base/starter/BaseStarterApplication.java
@@ -0,0 +1,21 @@
+package com.alinesno.infra.base.starter;
+
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 集成一个Java开发示例工具
+ * @author LuoAnDong
+ * @since 2023年8月3日 上午6:23:43
+ */
+@EnableInfraSsoApi
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class BaseStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseStarterApplication.class, args);
+	}
+
+}
