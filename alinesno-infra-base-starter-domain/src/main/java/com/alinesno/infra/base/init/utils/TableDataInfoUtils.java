b81521b95c0e39fb92d070a72116c5a96934450d
switchII <luoandon@gmail.com>
2023-09-30 16:54:07 +0800

    feat:集成ruoyi-plus代码生成器controller

--- alinesno-infra-base-starter-domain/src/main/java/com/alinesno/infra/base/init/utils/TableDataInfoUtils.java
+++ alinesno-infra-base-starter-domain/src/main/java/com/alinesno/infra/base/init/utils/TableDataInfoUtils.java
@@ -3,11 +3,9 @@ package com.alinesno.infra.base.init.utils;
 import cn.hutool.http.HttpStatus;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import org.jetbrains.annotations.NotNull;
 
 public class TableDataInfoUtils {
 
-    @NotNull
     public static <T> TableDataInfo build(IPage<T> page) {
 
         TableDataInfo rspData = new TableDataInfo();
