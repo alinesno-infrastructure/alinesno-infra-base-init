96769d3451085c0b20adbb9bdfc9870608eed1d0
CarrotSea233 <2733614757@qq.com>
2023-10-31 16:51:43 +0800

    fix: Delete the actable

--- alinesno-infra-base-starter-boot/src/main/java/com/alinesno/infra/base/starter/BaseStarterApplication.java
+++ alinesno-infra-base-starter-boot/src/main/java/com/alinesno/infra/base/starter/BaseStarterApplication.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.base.starter;
 
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import com.alinesno.infra.sdk.actable.enable.EnableActable;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author luoxiaodong
  * @since 2023年8月3日 上午6:23:43
  */
-@EnableActable
 @EnableInfraSsoApi
 @MapperScan({"com.alinesno.infra.base.starter.mapper"})
 @SpringBootApplication
