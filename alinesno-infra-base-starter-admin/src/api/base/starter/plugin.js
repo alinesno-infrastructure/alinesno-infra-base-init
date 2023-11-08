309d524b19d58bef5ffe93dc33eba41e2a23e8a3
CarrotSea233 <2733614757@qq.com>
2023-11-08 21:26:04 +0800

    feat: add plugin-ui

--- alinesno-infra-base-starter-admin/src/api/base/starter/plugin.js
+++ alinesno-infra-base-starter-admin/src/api/base/starter/plugin.js
@@ -1,135 +1,68 @@
 import request from '@/utils/request'
 import { parseStrEmpty } from "@/utils/ruoyi";
 
-// 查询用户列表
+/**
+ * 插件接口操作
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/base/starter/plugin/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'save' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  downloadfile: prefix + "downloadfile"
+}
+
+// 查询插件列表
 export function listPlugin(query) {
   return request({
-    url: '/api/infra/base/starter/plugin/datatables',
+    url: managerUrl.datatables ,
     method: 'post',
     params: query
   })
 }
 
-// 查询用户详细
-export function getPlugin(userId) {
+// 查询插件详细
+export function getPlugin(pluginId) {
   return request({
-    url: '/api/infra/base/starter/plugin/' + parseStrEmpty(userId),
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(pluginId),
     method: 'get'
   })
 }
 
-// 新增用户
+// 新增插件
 export function addPlugin(data) {
   return request({
-    url: '/api/infra/base/starter/plugin',
+    url: managerUrl.saveUrl ,
     method: 'post',
     data: data
   })
 }
 
-// 修改用户
+// 修改插件
 export function updatePlugin(data) {
   return request({
-    url: '/api/infra/base/starter/plugin',
+    url: managerUrl.updateUrl ,
     method: 'put',
     data: data
   })
 }
 
-// 删除用户
-export function delPlugin(userId) {
+// 删除插件
+export function delPlugin(pluginId) {
   return request({
-    url: '/api/infra/base/starter/plugin/' + userId,
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(pluginId),
     method: 'delete'
-  })
-}
-
-// 用户密码重置
-export function resetPluginPwd(userId, password) {
-  const data = {
-    userId,
-    password
-  }
-  return request({
-    url: '/api/infra/base/starter/plugin/resetPwd',
-    method: 'put',
-    data: data
-  })
-}
-
-// 用户状态修改
-export function changePluginStatus(userId, status) {
-  const data = {
-    userId,
-    status
-  }
-  return request({
-    url: '/api/infra/base/starter/plugin/changeStatus',
-    method: 'put',
-    data: data
-  })
-}
-
-// 查询用户个人信息
-export function getPluginProfile() {
-  return request({
-    url: '/api/infra/base/starter/plugin/profile',
-    method: 'get'
-  })
-}
-
-// 修改用户个人信息
-export function updatePluginProfile(data) {
-  return request({
-    url: '/api/infra/base/starter/plugin/profile',
-    method: 'put',
-    data: data
-  })
-}
-
-// 用户密码重置
-export function updatePluginPwd(oldPassword, newPassword) {
-  const data = {
-    oldPassword,
-    newPassword
-  }
-  return request({
-    url: '/api/infra/base/starter/plugin/profile/updatePwd',
-    method: 'put',
-    params: data
-  })
-}
-
-// 用户头像上传
-export function uploadAvatar(data) {
-  return request({
-    url: '/api/infra/base/starter/plugin/profile/avatar',
-    method: 'post',
-    data: data
-  })
-}
-
-// 查询授权角色
-export function getAuthRole(userId) {
-  return request({
-    url: '/api/infra/base/starter/plugin/authRole/' + userId,
-    method: 'get'
-  })
-}
-
-// 保存授权角色
-export function updateAuthRole(data) {
-  return request({
-    url: '/api/infra/base/starter/plugin/authRole',
-    method: 'put',
-    params: data
-  })
-}
-
-// 查询部门下拉树结构
-export function deptTreeSelect() {
-  return request({
-    url: '/api/infra/base/starter/plugin/deptTree',
-    method: 'get'
   })
 }
