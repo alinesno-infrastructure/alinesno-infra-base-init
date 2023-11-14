5b85018a4b1fcfdf6bbaa1407291fc9a88074047
Br <1945410340@qq.com>
2023-11-14 08:42:12 +0800

    调整ui

--- alinesno-infra-base-starter-admin/src/views/base/starter/application/index.vue
+++ alinesno-infra-base-starter-admin/src/views/base/starter/application/index.vue
@@ -6,12 +6,10 @@
         <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
 
           <el-form-item label="应用名称" prop="applicationName">
-            <el-input v-model="queryParams['condition[applicationName|like]']" placeholder="请输入应用名称" clearable
-                      style="width: 240px" @keyup.enter="handleQuery"/>
+            <el-input v-model="queryParams['condition[applicationName|like]']" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery"/>
           </el-form-item>
           <el-form-item label="显示名称" prop="showName" label-width="100px">
-            <el-input v-model="queryParams['condition[showName|like]']" placeholder="请输入显示名称" clearable
-                      style="width: 240px" @keyup.enter="handleQuery"/>
+            <el-input v-model="queryParams['condition[showName|like]']" placeholder="请输入显示名称" clearable style="width: 240px" @keyup.enter="handleQuery"/>
           </el-form-item>
 
           <el-form-item>
@@ -22,35 +20,13 @@
 
         <el-row :gutter="10" class="mb8">
           <el-col :span="1.5">
-            <el-button
-                type="primary"
-                plain
-                icon="Plus"
-                @click="handleAdd"
-            >新增
-            </el-button>
+            <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
           </el-col>
           <el-col :span="1.5">
-            <el-button
-                type="success"
-                plain
-                icon="Edit"
-                :disabled="single"
-                @click="handleUpdate"
-                v-hasPermi="['system:Application:edit']"
-            >修改
-            </el-button>
+            <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate">修改</el-button>
           </el-col>
           <el-col :span="1.5">
-            <el-button
-                type="danger"
-                plain
-                icon="Delete"
-                :disabled="multiple"
-                @click="handleDelete"
-                v-hasPermi="['system:Application:remove']"
-            >删除
-            </el-button>
+            <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
           </el-col>
 
           <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
@@ -58,20 +34,13 @@
 
         <el-table v-loading="loading" :data="ApplicationList" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="50" align="center"/>
-          <el-table-column label="图标" align="center" width="55px" prop="icon" v-if="columns[0].visible">
-          </el-table-column>
-          <el-table-column label="应用名称" align="center" key="applicationName" prop="applicationName"
-                           v-if="columns[1].visible" :show-overflow-tooltip="true"/>
-          <el-table-column label="显示名称" align="center" key="showName" prop="showName" v-if="columns[2].visible"
-                           :show-overflow-tooltip="true"/>
-          <el-table-column label="所属领域" align="center" key="domain" prop="domain" v-if="columns[3].visible"
-                           :show-overflow-tooltip="true"/>
-          <el-table-column label="域名" align="center" key="domainName" prop="domainName" v-if="columns[4].visible"
-                           :show-overflow-tooltip="true"/>
-          <el-table-column label="安全存储路径" align="center" key="storagePath" prop="storagePath"
-                           v-if="columns[5].visible" :show-overflow-tooltip="true"/>
-          <el-table-column label="应用目标" align="center" key="target" prop="target" v-if="columns[6].visible"
-                           :show-overflow-tooltip="true"/>
+          <el-table-column label="图标" align="center" width="55px" prop="icon" v-if="columns[0].visible"></el-table-column>
+          <el-table-column label="应用名称" align="center" key="applicationName" prop="applicationName" v-if="columns[1].visible" :show-overflow-tooltip="true"/>
+          <el-table-column label="显示名称" align="center" key="showName" prop="showName" v-if="columns[2].visible" :show-overflow-tooltip="true"/>
+          <el-table-column label="所属领域" align="center" key="domain" prop="domain" v-if="columns[3].visible" :show-overflow-tooltip="true"/>
+          <el-table-column label="域名" align="center" key="domainName" prop="domainName" v-if="columns[4].visible" :show-overflow-tooltip="true"/>
+          <el-table-column label="安全存储路径" align="center" key="storagePath" prop="storagePath" v-if="columns[5].visible" :show-overflow-tooltip="true"/>
+          <el-table-column label="应用目标" align="center" key="target" prop="target" v-if="columns[6].visible" :show-overflow-tooltip="true"/>
           <el-table-column label="创建时间" align="center" prop="addTime" v-if="columns[7].visible" width="160">
             <template #default="scope">
               <span>{{ parseTime(scope.row.addTime) }}</span>
@@ -79,25 +48,16 @@
           </el-table-column>
           <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width" v-if="columns[8].visible">
             <template #default="scope">
-              <el-tooltip content="修改" placement="top" v-if="scope.row.applicationId !== 1">
-                <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-                           v-hasPermi="['system:Application:edit']"></el-button>
+              <el-tooltip content="修改" placement="top" v-if="scope.row.id !== 1">
+                <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"></el-button>
               </el-tooltip>
-              <el-tooltip content="删除" placement="top" v-if="scope.row.applicationId !== 1">
-                <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
-                           v-hasPermi="['system:Application:remove']"></el-button>
+              <el-tooltip content="删除" placement="top" v-if="scope.row.id !== 1">
+                <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"></el-button>
               </el-tooltip>
-
             </template>
           </el-table-column>
         </el-table>
-        <pagination
-            v-show="total > 0"
-            :total="total"
-            v-model:page="queryParams.pageNum"
-            v-model:limit="queryParams.pageSize"
-            @pagination="getList"
-        />
+        <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList"/>
       </el-col>
     </el-row>
 
@@ -118,7 +78,6 @@
             </el-form-item>
           </el-col>
         </el-row>
-
         <el-row>
           <el-col :span="24">
             <el-form-item label="域名" prop="domainName">
@@ -133,7 +92,6 @@
             </el-form-item>
           </el-col>
         </el-row>
-
         <el-row>
           <el-col :span="24">
             <el-form-item label="安全存储路径" prop="storagePath" label-width="107px">
@@ -156,66 +114,22 @@
         </div>
       </template>
     </el-dialog>
-
-    <!-- 应用导入对话框 -->
-    <el-dialog :title="upload.title" v-model="upload.open" width="400px" append-to-body>
-      <el-upload
-          ref="uploadRef"
-          :limit="1"
-          accept=".xlsx, .xls"
-          :headers="upload.headers"
-          :action="upload.url + '?updateSupport=' + upload.updateSupport"
-          :disabled="upload.isUploading"
-          :on-progress="handleFileUploadProgress"
-          :on-success="handleFileSuccess"
-          :auto-upload="false"
-          drag
-      >
-        <el-icon class="el-icon--upload">
-          <upload-filled/>
-        </el-icon>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <template #tip>
-          <div class="el-upload__tip text-center">
-            <div class="el-upload__tip">
-              <el-checkbox v-model="upload.updateSupport"/>
-              是否更新已经存在的应用数据
-            </div>
-            <span>仅允许导入xls、xlsx格式文件。</span>
-            <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;"
-                     @click="importTemplate">下载模板
-            </el-link>
-          </div>
-        </template>
-      </el-upload>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitFileForm">确 定</el-button>
-          <el-button @click="upload.open = false">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
   </div>
 </template>
 
 <script setup name="Application">
-import {getToken} from "@/utils/auth";
+
 import {
-  changeApplicationStatus,
   listApplication,
-  resetApplicationPwd,
   delApplication,
   getApplication,
   updateApplication,
   addApplication,
-  deptTreeSelect
 } from "@/api/base/starter/application";
 import {reactive} from "vue";
 
 const router = useRouter();
 const {proxy} = getCurrentInstance();
-// const { sys_normal_disable, sys_Application_sex } = proxy.useDict("sys_normal_disable", "sys_Application_sex");
-
 const ApplicationList = ref([]);
 const open = ref(false);
 const loading = ref(true);
@@ -226,26 +140,7 @@ const multiple = ref(true);
 const total = ref(0);
 const title = ref("");
 const dateRange = ref([]);
-const deptName = ref("");
-const deptOptions = ref(undefined);
-const initPassword = ref(undefined);
-const postOptions = ref([]);
-const roleOptions = ref([]);
-/*** 应用导入参数 */
-const upload = reactive({
-  // 是否显示弹出层（应用导入）
-  open: false,
-  // 弹出层标题（应用导入）
-  title: "",
-  // 是否禁用上传
-  isUploading: false,
-  // 是否更新已经存在的应用数据
-  updateSupport: 0,
-  // 设置上传的请求头部
-  headers: {Authorization: "Bearer " + getToken()},
-  // 上传的地址
-  url: import.meta.env.VITE_APP_BASE_API + "/system/Application/importData"
-});
+
 // 列显隐信息
 const columns = ref([
   {key: 0, label: `图标`, visible: true},
@@ -272,13 +167,8 @@ const data = reactive({
     deptId: undefined
   },
   rules: {
-    applicationId: [{required: true, message: "应用编号不能为空", trigger: "blur"}],
-    applicationName: [{required: true, message: "应用名称不能为空", trigger: "blur"}, {
-      min: 2,
-      max: 20,
-      message: "应用名称长度必须介于 2 和 20 之间",
-      trigger: "blur"
-    }],
+    id: [{required: true, message: "应用编号不能为空", trigger: "blur"}],
+    applicationName: [{required: true, message: "应用名称不能为空", trigger: "blur"}, {min: 2, max: 20, message: "应用名称长度必须介于 2 和 20 之间", trigger: "blur"}],
     showName: [{required: true, message: "显示名称不能为空", trigger: "blur"}],
     domain: [{required: true, message: "所属领域不能为空", trigger: "blur"}],
     domainName: [{required: true, message: "域名不能为空", trigger: "blur"}],
@@ -289,7 +179,6 @@ const data = reactive({
 
 const {queryParams, form, rules} = toRefs(data);
 
-
 /** 查询应用列表 */
 function getList() {
   loading.value = true;
@@ -299,7 +188,6 @@ function getList() {
     total.value = res.total;
   });
 };
-
 
 /** 搜索按钮操作 */
 function handleQuery() {
@@ -314,16 +202,15 @@ function resetQuery() {
   proxy.resetForm("queryRef");
   queryParams.value.applicationName = undefined;
   queryParams.value.showName = undefined;
-  proxy.$refs.deptTreeRef.setCurrentKey(null);
   handleQuery();
 };
 
 /** 删除按钮操作 */
 function handleDelete(row) {
-  const applicationIds = row.id || ids.value;
+  const id = row.id || ids.value;
 
-  proxy.$modal.confirm('是否确认删除应用编号为"' + applicationIds + '"的数据项？').then(function () {
-    return delApplication(applicationIds);
+  proxy.$modal.confirm('是否确认删除应用编号为"' + id + '"的数据项？').then(function () {
+    return delApplication(id);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
@@ -341,7 +228,7 @@ function handleSelectionChange(selection) {
 /** 重置操作表单 */
 function reset() {
   form.value = {
-    applicationId: undefined,
+    id: undefined,
     applicationName: undefined,
     showName: undefined,
     domain: undefined,
@@ -368,10 +255,9 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
   reset();
-  const applicationId = row.id || ids.value;
-  getApplication(applicationId).then(response => {
+  const id = row.id || ids.value;
+  getApplication(id).then(response => {
     form.value = response.data;
-    form.value.applicationId = applicationId;
     open.value = true;
     title.value = "修改应用";
 
@@ -382,7 +268,7 @@ function handleUpdate(row) {
 function submitForm() {
   proxy.$refs["ApplicationRef"].validate(valid => {
     if (valid) {
-      if (form.value.applicationId != undefined) {
+      if (form.value.id != undefined) {
         updateApplication(form.value).then(response => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
