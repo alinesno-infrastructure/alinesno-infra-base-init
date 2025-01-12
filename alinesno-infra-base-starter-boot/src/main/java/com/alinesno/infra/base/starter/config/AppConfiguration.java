c5d646e37f56d52b4c97d69189ebde63c45cbaf9
陈伟(Victor) <victorAip@gmail.com>
2025-01-13 00:10:53 +0800

    feat:在AppConfiguration中添加CommandLineRunner实现及日志记录

--- alinesno-infra-base-starter-boot/src/main/java/com/alinesno/infra/base/starter/config/AppConfiguration.java
+++ alinesno-infra-base-starter-boot/src/main/java/com/alinesno/infra/base/starter/config/AppConfiguration.java
@@ -1,14 +1,33 @@
 package com.alinesno.infra.base.starter.config;
 
+import com.alinesno.infra.common.core.auto.EnableCore;
+import com.alinesno.infra.common.extend.datasource.enable.EnableInfraDataScope;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 应用配置
+ */
+@Slf4j
+@EnableInfraDataScope
 @EnableActable
+@EnableCore
 @EnableInfraSsoApi
 @MapperScan({"com.alinesno.infra.base.starter.mapper"})
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer" ,
+        "com.alinesno.infra.base.platform.adapter"
+})
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements CommandLineRunner {
 
+    @Override
+    public void run(String... args) throws Exception {
+       log.debug("Application started successfully.");
+    }
 }
